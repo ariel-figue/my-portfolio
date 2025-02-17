fd52b1c04e4f3759cf708a51e6e5fd17cc5b57b9
Ariel Figueroa <arielblue721@gmail.com>
2025-02-17 14:52:07 -0500

    prevent scroll while image is open.

--- src/components/ImageViewer.tsx
+++ src/components/ImageViewer.tsx
@@ -37,6 +37,14 @@ const ImageViewer = ({
     };
   }, [isOpen]);
 
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'auto';
+    }
+  }, [isOpen]);
+
   const openModal = (imageSrc: string | StaticImageData) => {
     setSelectedImage(imageSrc);
     setIsOpen(true);
