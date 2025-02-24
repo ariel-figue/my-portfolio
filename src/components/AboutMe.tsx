400ffecd4150ed85303e5be68277df70c8fe92b0
Ariel Figueroa <arielblue721@gmail.com>
2025-02-24 12:51:19 -0500

    updated about me

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -5,28 +5,33 @@ import FadeInSection from "./FadeInSection";
 // AboutMe Section
 export const AboutMeContent = () => (
   <FadeInSection>
-    <h2 className="text-3xl font-bold text-left text-[#2c3e50] mb-2">About Me</h2>
+    <h2 className="text-3xl font-bold text-left text-[#2c3e50] mb-2">
+      About Me
+    </h2>
     <div className="max-w-5xl">
       <p>
         I&apos;m a first-generation Cuban-American and a Senior Front-End
         Engineer passionate about building intuitive, high-performance web
         applications.
       </p>
-      <p className="mt-4 ">
+      <p className="mt-4">
         I&apos;m also a proud Florida International University (FIU) Alumni,
-        Class of 2018. With expertise in React, TypeScript, and modern
-        front-end architecture, I craft seamless user experiences that balance
+        Class of 2018. With expertise in React, TypeScript, and modern front-end
+        architecture, I craft seamless user experiences that balance
         functionality and aesthetics. Whether it&apos;s optimizing performance,
         designing scalable UI components, or integrating cutting-edge
         technologies, I thrive on turning ideas into polished digital products.
-      </p>
-      <p>
-        Explore my work, and feel free to reach out—I&apos;d love to connect!
+        My passion for innovative software engineering is fueled by my love for
+        creating solutions to real-world problems and continuously building and
+        improving impactful tools.
       </p>
       <p className="mt-4">
         Beyond coding, I&apos;m a travel enthusiast with a love for the great
         outdoors. You can find me camping, fishing, or simply enjoying the
         beach.
+      </p>
+      <p className="mt-4">
+        Explore my work, and feel free to reach out—I&apos;d love to connect!
       </p>
     </div>
   </FadeInSection>
